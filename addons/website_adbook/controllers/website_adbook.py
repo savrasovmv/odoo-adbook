215a1129a546366822562da735fde31913c7f56f
savrasovmv <savrasovmv@tmenergo.ru>
2021-09-22 11:07:42 +0500

    wadbook

--- addons/website_adbook/controllers/website_adbook.py
+++ addons/website_adbook/controllers/website_adbook.py
@@ -126,6 +126,7 @@ class AdBook(http.Controller):
 
     @http.route(['/wadbook','/wadbook/<int:department_id>'], type='http', auth="user", website=True, sitemap=True)
     def index1(self, department_id=False, **kw):
+        
         branch_list = http.request.env['ad.branch'].sudo().search([
                                 ('active', '=', True),
                                 ('is_view_adbook', '=', True),
@@ -135,11 +136,13 @@ class AdBook(http.Controller):
                                                         ('active', '=', True),
                                                         ('is_view_adbook', '=', True),
                                                     ], 
-                                                        order="sequence asc"
+                                                        order="name"
                                                     )
         employer_list = []
+        current_department_name = ''
 
         if department_id:
+            is_homepage = False
             employer_list = http.request.env['ad.users'].sudo().search([
                                     # ('branch_id', '=', branch_id.id),
                                     ('active', '=', True),
@@ -148,55 +151,84 @@ class AdBook(http.Controller):
                                 ], order="sequence asc")
             current_department = http.request.env['ad.department'].sudo().browse([department_id])
             branch_id = current_department.branch_id
+            current_department_name = current_department.name
+        else:
+            is_homepage = True
         
         if not branch_list:
             return "Нет ниодного подразделения для отображения в справочнике. Установите хотя бы для одного объекта Подразделения AD 'Отображать в справочнике контктов'"
 
-        # if branch_id in branch_list.ids:
-        #     branch_id = http.request.env['ad.branch'].sudo().browse(branch_id)
-        
+               
         if branch_list and not department_id:
             branch_id = branch_list[0]
 
-        # if branch_id:
-        #     department_list_id = http.request.env['ad.users'].sudo().read_group([ 
-        #                                                 # ('branch_id', '=', branch_id.id),
-        #                                                 ('active', '=', True),
-        #                                                 ('is_view_adbook', '=', True),
-        #                                             ], 
-        #                                                 fields=['department_id'], 
-        #                                                 groupby=['department_id']
-        #                                             )
-        #     department_ids = []
+        # import random
+        # #Generate 5 random numbers between 10 and 30
+        # randomlist = []
+        # if len(employer_list)>0:
+        #     randomlist = random.sample(range(0, 5), len(employer_list)+1)
 
-
-            
-            # for data in department_list_id:
-            #     d_id, obj = data['department_id']
-            #     department_ids.append(d_id)
-           
-            # department_list = http.request.env['ad.department'].sudo().search([ 
-            #                                             ('id', 'in', department_ids),
-            #                                             ('active', '=', True),
-            #                                             ('is_view_adbook', '=', True),
-            #                                         ], 
-            #                                             order="sequence asc"
-            #                                         )
-            # employer_list = http.request.env['ad.users'].sudo().search([
-            #                     ('branch_id', '=', branch_id.id),
-            #                     ('active', '=', True),
-            #                     ('is_view_adbook', '=', True),
-            #                 ], order="sequence asc")
-        # else:
-        #     employer_list = []
-        #     branch_list = []
-        #     department_list = []
-        #     branch_id = ''
             
         return http.request.render('website_adbook.website_index', {
             'search': False,
+            'is_homepage': is_homepage,
             'current_branch_id': branch_id,
-            'current_department_name': current_department.name,
+            'current_department_name': current_department_name,
+            'branch_list':  branch_list,
+            'department_list':  department_list,
+            'employer_list':  employer_list,
+            # 'randomlist': randomlist,
+        })
+
+
+    @http.route(['/wadbook/search'], type='http', auth="user", website=True, sitemap=True)
+    def wsearch(self, search=False, **kw):
+
+        if not search or search=='':
+            return http.request.redirect('/wadbook')
+
+        
+        branch_list = http.request.env['ad.branch'].sudo().search([
+                                ('active', '=', True),
+                                ('is_view_adbook', '=', True),
+                            ], order="sequence asc")
+
+        department_list = http.request.env['ad.department'].sudo().search([ 
+                                                        ('active', '=', True),
+                                                        ('is_view_adbook', '=', True),
+                                                    ], 
+                                                        order="name"
+                                                    )
+
+        limit = 50
+        employer_list = http.request.env['ad.users'].sudo().search([
+                            ('search_text', '=ilike', search+'%'),
+                            ('active', '=', True),
+                            ('is_view_adbook', '=', True),
+                            ('branch_id', 'in', branch_list.ids),
+                        ], order="name asc", limit=limit)
+        
+        limit2 = limit - len(employer_list)
+        employer_list += http.request.env['ad.users'].sudo().search([
+                            ('search_text', 'ilike', search),
+                            ('id', 'not in', employer_list.ids),
+                            ('active', '=', True),
+                            ('is_view_adbook', '=', True),
+                            ('branch_id', 'in', branch_list.ids),
+                        ], order="name asc", limit=limit2)
+
+                    
+        
+        if not branch_list:
+            return "Нет ниодного подразделения для отображения в справочнике. Установите хотя бы для одного объекта Подразделения AD 'Отображать в справочнике контктов'"
+            
+        return http.request.render('website_adbook.website_index', {
+            'search': True,
+            'search_text': search,
+            'is_limit': True if limit == len(employer_list) else False,
+            'is_homepage': False,
+            'current_branch_id': False,
+            'current_department_name': False,
             'branch_list':  branch_list,
             'department_list':  department_list,
             'employer_list':  employer_list,
